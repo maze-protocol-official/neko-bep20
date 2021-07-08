c66e619efe13323121dcbe057ed446decad0fc76
yunmin <79829820+Hacker-Yun@users.noreply.github.com>
2021-07-08 10:40:43 +0800

    Update OwnerNeko.sol

--- contracts/OwnerNeko.sol
+++ contracts/OwnerNeko.sol
@@ -5,32 +5,32 @@ import { NEKOToken } from './NEKOToken.sol';
 import { SafeMath } from './SafeMath.sol';
 
 contract OwnerNeko {
-    
+
     using SafeMath for uint256;
-    
+
     // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
     bytes32 public constant PERMIT_TYPEHASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
     string public constant name = 'Neko Mint';
     uint32 public constant LOCK_PERIOD =  48 hours;
     uint32 public constant PERIOD = 72 hours;
     uint256 public constant MAX_MINT_COUNT = 504000 * 10 ** 18;
-    
+
     bytes32 public DOMAIN_SEPARATOR;
-    
+
     address[] public permitOwners;
     uint256 public ownerCount;
     uint256 public ownerRight;
-    
+
     uint public blockTimestampLast;
-    
+
     NEKOToken public neko;
 
     Data public data;
 
     mapping(address => uint256) public ownerNonce;
-    
+
     enum Type { Empty, LockMint, UnLockMint, AddOwner, RemoveOwner, ChangeOwner, Recall }
-    
+
     struct Data {
         address[] sigOwners;
         uint256 budgetMint;
@@ -38,42 +38,42 @@ contract OwnerNeko {
         address spender;
         uint256 state;
     }
-    
+
     event AddPermitOwners(address newOwner);
-    
+
     event RemovePermitOwners(address oldOwner);
-    
+
     event LockMint(uint256 amount);
-    
+
     event UnLockMint(address spender, uint256 amount);
-    
+
     event ChangeOwner(address owner);
-    
+
     event Recall(address owner, uint state);
-    
+
     modifier CheckState(uint256 _state) {
         require(data.state == _state || data.state == uint(Type.Empty), "The last signature is not finished");
         _;
     }
-    
+
     modifier CheckPrestrain() {
         require(data.blockTimestampLockLast == 0 || data.budgetMint == 0, "Prestrain cannot be repeated");
-         _;
+        _;
     }
-    
+
     modifier CheckAddress(address _owner) {
         require(_owner != address(0), "Invalid address");
         _;
     }
-    
+
     constructor
     (
         address _neko,
         address[] memory _owners
     ) {
-        
+
         neko = NEKOToken(_neko);
-        
+
         ownerCount = _owners.length;
         require(ownerCount > 0, "Owners exception");
         for (uint x = 0; x < ownerCount; x++) {
@@ -81,13 +81,13 @@ contract OwnerNeko {
             permitOwners.push(_owners[x]);
         }
         setOwenerRight();
-        
+
         uint256 chainId;
 
         assembly {
             chainId := chainid()
         }
-        
+
         DOMAIN_SEPARATOR = keccak256(
             abi.encode(
                 keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)'),
@@ -98,9 +98,9 @@ contract OwnerNeko {
             )
         );
     }
-    
-    function getData() external view 
-    returns 
+
+    function getData() external view
+    returns
     (
         address[] memory sigOwners,
         uint256 budgetMint,
@@ -114,7 +114,7 @@ contract OwnerNeko {
         spender = data.spender;
         state = data.state;
     }
-    
+
     function setOwenerRight() internal {
         if (ownerCount == 1) {
             ownerRight = 1;
@@ -122,43 +122,43 @@ contract OwnerNeko {
             ownerRight = ownerCount.mul(2).div(3);
         }
     }
-    
+
     function addPermitOwners
     (
         address owner,
-        address spender, 
-        uint value, 
-        uint deadline, 
-        uint8 v, 
-        bytes32 r, 
+        address spender,
+        uint value,
+        uint deadline,
+        uint8 v,
+        bytes32 r,
         bytes32 s
     ) external CheckAddress(owner) CheckState(uint(Type.AddOwner)) {
         require(!checkPermitOwners(spender), "Already existed");
-        
+
         bool result = permit(uint(Type.AddOwner), owner, spender, value, deadline, v, r, s);
         if (result) {
             permitOwners.push(spender);
             ownerCount = permitOwners.length;
             setOwenerRight();
-            
+
             delete data;
-            
+
             emit AddPermitOwners(spender);
         }
     }
-    
+
     function removePermitOwners
     (
         address owner,
-        address spender, 
-        uint value, 
-        uint deadline, 
-        uint8 v, 
-        bytes32 r, 
+        address spender,
+        uint value,
+        uint deadline,
+        uint8 v,
+        bytes32 r,
         bytes32 s
     ) external CheckState(uint(Type.RemoveOwner)) {
         require(ownerCount > 1, "Not least one");
-        
+
         bool result = permit(uint(Type.RemoveOwner), owner, spender, value, deadline, v, r, s);
         if (result) {
             for (uint256 x = 0; x < ownerCount; x++) {
@@ -169,25 +169,36 @@ contract OwnerNeko {
                     permitOwners.pop();
                     ownerCount = permitOwners.length;
                     setOwenerRight();
-                    
+
                     delete data;
-                    
+
                     emit RemovePermitOwners(spender);
                     break;
                 }
             }
         }
     }
-    
-    function checkSigOwners (address[] memory owners, address owner) internal pure {
+
+    function checkSigOwners (address[] memory owners, address owner, uint state) internal pure {
         uint length = owners.length;
         if (length > 0) {
-            for (uint256 x = 0; x < length; x++) {
-                require(owners[x] != owner, "Already existed");
+            if (state != uint(Type.Recall)) {
+                for (uint256 x = 0; x < length; x++) {
+                    require(owners[x] != owner, "Already existed");
+                }
+            } else {
+                uint count;
+                for (uint256 x = 0; x < length; x++) {
+                    if (owners[x] == owner) {
+                        count++;
+                    }
+                }
+                require(count == 1, "Abnormal error");
             }
+
         }
     }
-    
+
     function checkPermitOwners (address owner) internal view returns (bool) {
         for (uint256 x = 0; x < permitOwners.length; x++) {
             if (permitOwners[x] == owner) {
@@ -196,35 +207,32 @@ contract OwnerNeko {
         }
         return false;
     }
-    
+
     function recallOwner (address owner) internal {
-        uint count;
         for (uint256 x = 0; x < data.sigOwners.length; x++) {
             if (data.sigOwners[x] == owner) {
-                count++;
                 for (uint i = x; i < data.sigOwners.length - 1; i++) {
                     data.sigOwners[i] = data.sigOwners[i + 1];
                 }
                 data.sigOwners.pop();
             }
         }
-        require(count > 0, "Abnormal error");
     }
-    
+
     function permit
     (
         uint256 state,
         address owner,
-        address spender, 
-        uint value, 
-        uint deadline, 
-        uint8 v, 
-        bytes32 r, 
+        address spender,
+        uint value,
+        uint deadline,
+        uint8 v,
+        bytes32 r,
         bytes32 s
     ) internal returns (bool) {
         require(deadline >= block.timestamp, "Invalid expiration");
         require(checkPermitOwners(owner), "Invalid owner");
-        checkSigOwners(data.sigOwners, owner);
+        checkSigOwners(data.sigOwners, owner, state);
         bytes32 digest = keccak256(
             abi.encodePacked(
                 '\x19\x01',
@@ -234,9 +242,10 @@ contract OwnerNeko {
         );
         address recoveredAddress = ecrecover(digest, v, r, s);
         if (recoveredAddress != address(0) && recoveredAddress == owner ) {
-            data.sigOwners.push(owner);
-            uint256 sigcount = data.sigOwners.length;
+            uint256 sigcount;
             if (state == uint(Type.LockMint)) {
+                data.sigOwners.push(owner);
+                sigcount = data.sigOwners.length;
                 if (sigcount == 1) {
                     data.budgetMint = value;
                     data.state = state;
@@ -246,6 +255,8 @@ contract OwnerNeko {
             } else if (state == uint(Type.Recall)) {
                 recallOwner(owner);
             } else {
+                data.sigOwners.push(owner);
+                sigcount = data.sigOwners.length;
                 if (sigcount == 1) {
                     data.spender = spender;
                     data.state = state;
@@ -265,95 +276,94 @@ contract OwnerNeko {
     function lockMint
     (
         address owner,
-        address spender, 
-        uint value, 
-        uint deadline, 
-        uint8 v, 
-        bytes32 r, 
+        address spender,
+        uint value,
+        uint deadline,
+        uint8 v,
+        bytes32 r,
         bytes32 s
     ) external CheckPrestrain CheckState(uint(Type.LockMint)) {
         require(value <= MAX_MINT_COUNT && value > 0, "Abnormal amount");
         uint256 timeElapsed = block.timestamp - blockTimestampLast;
         require(timeElapsed >= PERIOD, "No satisfaction time");
-        
+
         bool result = permit(uint(Type.LockMint), owner, spender, value, deadline, v, r, s);
         if (result) {
             data.blockTimestampLockLast = block.timestamp;
             data.state = uint(Type.Empty);
+
             delete  data.sigOwners;
-            
             emit LockMint(value);
         }
-        
+
     }
-    
+
     function unlockMint
     (
         address owner,
-        address spender, 
-        uint value, 
-        uint deadline, 
-        uint8 v, 
-        bytes32 r, 
+        address spender,
+        uint value,
+        uint deadline,
+        uint8 v,
+        bytes32 r,
         bytes32 s
     ) external CheckAddress(spender) CheckState(uint(Type.UnLockMint)) {
         uint block_time = block.timestamp;
         uint256 timeElapsed = block_time - data.blockTimestampLockLast;
         require(timeElapsed >= LOCK_PERIOD, "No satisfaction time");
-        
+
         bool result = permit(uint(Type.UnLockMint), owner, spender, value, deadline, v, r, s);
         if (result) {
             neko.mint(data.spender, data.budgetMint);
-            
+
             delete data;
-            
+
             blockTimestampLast = block_time;
-            
+
             emit UnLockMint(data.spender, data.budgetMint);
         }
     }
-    
+
     function changeOwner
     (
         address owner,
-        address spender, 
-        uint value, 
-        uint deadline, 
-        uint8 v, 
-        bytes32 r, 
+        address spender,
+        uint value,
+        uint deadline,
+        uint8 v,
+        bytes32 r,
         bytes32 s
     ) external CheckAddress(owner) CheckState(uint(Type.ChangeOwner)) {
-        
+
         bool result = permit(uint(Type.ChangeOwner), owner, spender, value, deadline, v, r, s);
         if (result) {
-            neko.transferOwnership(spender); 
+            neko.transferOwnership(spender);
 
             delete data;
-            
+
             emit ChangeOwner(spender);
         }
     }
-    
-    function recall 
+
+    function recall
     (
         uint state,
         address owner,
-        address spender, 
-        uint value, 
-        uint deadline, 
-        uint8 v, 
-        bytes32 r, 
+        address spender,
+        uint value,
+        uint deadline,
+        uint8 v,
+        bytes32 r,
         bytes32 s
     ) external CheckState(state) {
-        
+
         permit(uint(Type.Recall), owner, spender, value, deadline, v, r, s);
-        
+
         if (data.sigOwners.length == 0) {
-            
+
             delete data;
-            
+
             emit Recall(owner, state);
         }
     }
-    
 }
